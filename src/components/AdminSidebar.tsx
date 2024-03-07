75c5951956b58465932f8e50a320593ed66fb603
DevRowdra <rowdradev23@gmail.com>
2024-03-06 20:28:31 -0800

    work for dashboard search bar and user profile

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,8 +1,9 @@
 
 import { IconType } from "react-icons"
 import {  AiFillFileText } from "react-icons/ai"
+import { FaChartBar, FaChartLine, FaChartPie, FaGamepad, FaStopwatch } from "react-icons/fa"
 import { IoIosPeople } from "react-icons/io"
-import { RiDashboardFill, RiShoppingBag2Fill } from "react-icons/ri"
+import { RiCoupon2Fill, RiDashboardFill, RiShoppingBag2Fill } from "react-icons/ri"
 import { Link,  Location,  useLocation } from "react-router-dom"
 
 const AdminSidebar = () => {
@@ -11,32 +12,51 @@ const AdminSidebar = () => {
   return (
     <aside>
         <h2>Logo..</h2>
-        <div>
-            <h5>Dashboard</h5>
-            <ul>
-              <Li url="/admin/dashboard" text="Dashboard" Icon={RiDashboardFill} location={location} ></Li>
-              <Li url="/admin/products" text="Products" Icon={RiShoppingBag2Fill} location={location} ></Li>
-               
-              <Li url="/admin/customers" text="Customers" Icon={IoIosPeople} location={location} ></Li>
-              <Li url="/admin/transaction" text="Transactions" Icon={AiFillFileText} location={location} ></Li>
-              
-            </ul> 
-        </div>
+       <DivOne location={location}/>
         {/* Charts */}
-        <div>
-            <h5>Charts</h5>
-            <ul>
-              <Li url="/admin/dashboard" text="Bar" Icon={RiDashboardFill} location={location} ></Li>
-              <Li url="/admin/products" text="Pie" Icon={RiShoppingBag2Fill} location={location} ></Li>
-               
-              <Li url="/admin/customers" text="Line " Icon={IoIosPeople} location={location} ></Li>
-              <Li url="/admin/transaction" text="Transactions" Icon={AiFillFileText} location={location} ></Li>
-              
-            </ul> 
-        </div>
+        <DivTwo location={location}/>
+
+        <DivThree location={location}/>
     </aside>
   )
 }
+
+const DivOne=({location}:{location:Location})=>( <div>
+  <h5>Dashboard</h5>
+  <ul>
+    <Li url="/admin/dashboard" text="Dashboard" Icon={RiDashboardFill} location={location} ></Li>
+    <Li url="/admin/products" text="Products" Icon={RiShoppingBag2Fill} location={location} ></Li>
+     
+    <Li url="/admin/customers" text="Customers" Icon={IoIosPeople} location={location} ></Li>
+    <Li url="/admin/transaction" text="Transactions" Icon={AiFillFileText} location={location} ></Li>
+    
+  </ul> 
+  </div>
+  )
+
+const DivTwo=({location}:{location:Location})=>(<div>
+  <h5>Charts</h5>
+  <ul>
+    <Li url="/admin/chart/bar" text="Bar" Icon={FaChartBar} location={location} ></Li>
+    <Li url="/admin/chart/pie" text="Pie" Icon={FaChartPie} location={location} ></Li>
+     
+    <Li url="/admin/chart/line" text="Line " Icon={FaChartLine} location={location} ></Li>
+   
+    
+  </ul> 
+</div>)
+const DivThree=({location}:{location:Location})=>(<div>
+  <h5>Apps</h5>
+  <ul>
+    <Li url="/admin/app/Stopwatch" text="Stopwatch" Icon={FaStopwatch} location={location} ></Li>
+    <Li url="/admin/app/coupon" text="Coupon" Icon={RiCoupon2Fill} location={location} ></Li>
+     
+    <Li url="/admin/app/toss" text="Toss " Icon={FaGamepad} location={location} ></Li>
+  
+    
+  </ul> 
+</div>)
+
 interface LiProps {
   url:string,
   location: Location
--- src/pages/Deshboard.tsx
+++ src/pages/Deshboard.tsx
@@ -1,11 +1,22 @@
+import { BsSearch } from "react-icons/bs"
 import AdminSidebar from "../components/AdminSidebar"
+import { FaRegBell } from "react-icons/fa"
 
+
+
+// const userImg='https://cdn.vectorstock.com/i/1000x1000/66/56/man-face-emotive-icon-in-flat-style-vector-13466656.webp'
 const Deshboard = () => {
   return (
     <div className="adminContainer" >
 <AdminSidebar></AdminSidebar>
-      <main>
-6.09 er 
+      <main className="dashboard">
+<div className="bar">
+  <BsSearch/>
+  <input type="text" placeholder="Search for data"/>
+  <FaRegBell/>
+  {/* <img src={userImg} alt="user"  /> */}
+</div>
+1.15 min for video
       </main>
     </div>
   )
